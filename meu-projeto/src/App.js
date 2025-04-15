0ab61b0e5518b7eba41181207edebf7bb4b3092a
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2025-04-15 11:41:07 -0300

    📦 Projeto Node.js criado

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Meu primeiro componente</h1>
+      <p>Olá Fatec</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ meu-projeto/src/Relogio.js
@@ -0,0 +1,10 @@
+import './Relogio.css';
+
+export default function Relogio() {
+  return (
+    <div>
+      <h1>Relógio</h1>
+      <p>Meu relógio</p>
+    </div>
+  );
+}
