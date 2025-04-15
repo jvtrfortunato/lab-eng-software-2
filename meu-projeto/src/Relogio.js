f3aff36e13a3b8efb909d7065fce15198782e16b
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2025-04-15 12:04:23 -0300

    📚 Implementação dos exercícios da aula 8

--- meu-projeto/src/Relogio.js
+++ meu-projeto/src/Relogio.js
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import './Relogio.css';
 
 export default function Relogio() {
+  const [horaAtual, setHoraAtual] = useState(new Date());
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setHoraAtual(new Date());
+    }, 1000); // Atualiza a cada segundo
+
+    return () => clearInterval(intervalo); // Limpa o intervalo ao desmontar
+  }, []);
+
+  const horaFormatada = horaAtual.toLocaleTimeString();
+
   return (
-    <div>
-      <h1>Relógio</h1>
-      <p>Meu relógio</p>
+    <div className="relogio">
+      <h2>{horaFormatada}</h2>
     </div>
   );
 }
+
