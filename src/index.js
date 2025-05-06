297c8d27b2897a8b10d64c234cc1aee5b22c58da
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2025-05-06 11:15:18 -0300

    Ajustes nas atividades 1 e 2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Letreiro from './Letreiro';
-import Relogio from './Relogio';
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Letreiro />
-    <Relogio />
+    <App />
   </React.StrictMode>
 );
 
